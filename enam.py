4aaf7b5f2602dfd1e78d14f82c06a59a56e20293
kertajasamuda <160152667+kertajasamuda@users.noreply.github.com>
2024-05-05 05:20:24 +0700

    Create enam.py

---
+++ enam.py
@@ -0,0 +1,168 @@
+from selenium import webdriver		
+from selenium.webdriver.chrome.options import Options		
+from selenium.webdriver import ActionChains		
+from selenium.webdriver.common.keys import Keys		
+from selenium.webdriver.support.ui import WebDriverWait		
+from selenium.webdriver.common.action_chains import ActionChains		
+from selenium.webdriver.common.by import By		
+from selenium.webdriver.support import expected_conditions as EC		
+from selenium.webdriver.common.by import By		
+from time import sleep		
+import string		
+import random		
+import copy		
+import sys		
+import subprocess		
+import zipfile		
+from config import	mail51	
+from config import	mail52	
+from config import	mail53	
+from config import	mail54	
+from config import	mail55	
+from config import	mail56	
+from config import	mail57	
+from config import	mail58	
+from config import	mail59	
+from config import	mail60	
+from config import password		
+#from scmining import script		
+#PROXY = "socks5://159.223.88.53:33333"		
+options = webdriver.ChromeOptions()		
+options.add_experimental_option("excludeSwitches", ["enable-automation"])		
+options.add_experimental_option('useAutomationExtension', False)		
+options.add_argument('--disable-blink-features=AutomationControlled')		
+options.add_argument("--window-size=600,700")		
+#options.add_argument('--proxy-server=%s' % PROXY)		
+#options.add_extension('anticaptcha-plugin_v0.62.zip')		
+driver = webdriver.Chrome(options=options)		
+sleep(1)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail51	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail52	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail53	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail54	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail55	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail56	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail57	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail58	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail59	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
+driver.implicitly_wait(1200) # seconds		
+driver.get("https://accounts.google.com/AddSession?service=accountsettings&continue=https://myaccount.google.com/?utm_source%3Dsign_in_no_continue&ec=GAlAwAE")		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierId"]').send_keys(	mail60	)
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="identifierNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button/span').click()		
+sleep(0.5)		
+driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').click()		
+sleep(0.5)		
