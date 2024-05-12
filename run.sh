f360479d5ac0b7dc9cd9094e96402c2c7778a20e
kertajasamuda <160152667+kertajasamuda@users.noreply.github.com>
2024-05-12 12:51:06 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 python satu.py >/dev/null & sleep 2
 python dua.py >/dev/null & sleep 2
-python tiga.py >/dev/null & sleep 2
-python empat.py >/dev/null & sleep 2
-python lima.py >/dev/null & sleep 2
 
