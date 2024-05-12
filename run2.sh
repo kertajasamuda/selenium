dfc24214d6108b38340305cedcfe2cfd415b9bef
kertajasamuda <160152667+kertajasamuda@users.noreply.github.com>
2024-05-12 12:44:51 +0700

    Create run2.sh

---
+++ run2.sh
@@ -0,0 +1,5 @@
+python enam.py >/dev/null & sleep 2
+python tujuh.py >/dev/null & sleep 2
+python delapan.py >/dev/null & sleep 2
+python sembilan.py >/dev/null & sleep 2
+python sepuluh.py >/dev/null & sleep 2
