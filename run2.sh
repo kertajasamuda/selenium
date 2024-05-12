dfa143dcbb579913f243550e61105d56ea11c92f
kertajasamuda <160152667+kertajasamuda@users.noreply.github.com>
2024-05-12 12:51:31 +0700

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,5 +1,3 @@
-python enam.py >/dev/null & sleep 2
-python tujuh.py >/dev/null & sleep 2
-python delapan.py >/dev/null & sleep 2
-python sembilan.py >/dev/null & sleep 2
-python sepuluh.py >/dev/null & sleep 2
+python tiga.py >/dev/null & sleep 2
+python empat.py >/dev/null & sleep 2
+
